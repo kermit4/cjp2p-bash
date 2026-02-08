14f08c287facef06cfa7045e20a6b458f26d8068
Christopher Pearson <kermit4@gmail.com>
2026-02-08 13:17:06 +0000

    not faster but i tried

--- Makefile
+++ Makefile
@@ -9,4 +9,4 @@ check:
 	cargo check
 
 test: release
-	./cjp2p.bash  32M $$((1<<25))
+	./cjp2p.bash  cb407d7355bb63929d7f4b282684f5a2884a0c3fb73d56642455600569a6888b  $$((1<<28)) # 256M
--- cjp2p.bash
+++ cjp2p.bash
@@ -4,8 +4,8 @@ export RUST_BACKTRACE=1
 export  RUST_LOG=warn
 coproc ./target/release/cjp2p-bash
 cd /dev/shm
-mkdir -p ${0##*/}
-cd ${0##*/}
+mkdir -p ${0##*/}.dir
+cd ${0##*/}.dir
 exec 10<&${COPROC[0]}    
 exec 11<&${COPROC[1]}
 { 
@@ -19,8 +19,8 @@ mkdir -p incoming/$1 # this starts the transfer
 BLOCK_SIZE=$((0xa000))
 req() {
     ((offset_wanted<eof)) && {
-            echo requesting $offset_wanted >&2
-            echo 159.69.54.127:24254 
+            #echo requesting $offset_wanted >&2
+            echo ${src:-159.69.54.127:24254}
             message="[{\"PleaseSendContent\":{\"id\":\"$id\",\"length\":$BLOCK_SIZE,\"offset\":$offset_wanted}}]"
             let offset_wanted+=$BLOCK_SIZE
             while [[ -s incoming/$id/$offset_wanted ]];do 
@@ -29,11 +29,11 @@ req() {
             echo ${#message}
             echo -n "$message"
         } >&11
-    true
+    [ . ]
 }
-id=$(ls incoming/|head -1)
+id=$(ls incoming/|head -1) # currrently does only one file at a time
 while true;do
-    if ! read -t 1  src <&10    ;then # bump
+    if ! read -rt 1  src <&10    ;then # bump
         [[ $id ]] && {
             ((offset_wanted>eof)) && 
             (($(find incoming/$id/ -mindepth 1 |wc -l) < $(((eof+BLOCK_SIZE-1)/BLOCK_SIZE)))) && {  # missing parts
@@ -50,26 +50,20 @@ while true;do
         }
         continue
     fi
-    read len <&10
-    messages=$(mktemp -p . messages.XXXXXXXXX)
+    read -r len <&10
+    messages=$(mktemp -p . m.XXXX)
     head -c $len <&10 > $messages
-#    jq -C < messages.json
+#    jq -C < $messages
 #    [{"Content":{"base64":"vLTtmB1Ot1dumq1Hscila3uKZF71KU2E3mDH","eof":1073741824,"id":"1024M","offset":204791808}}]
-    { 
-        content=$(mktemp -p . content.XXXXXXXXX)
-        if jq -re '.[].Content'  < $messages > $content ;then 
-            offset_in=$( jq -r '.offset' < $content )
-            echo received $offset_in >&2 window $((offset_wanted - $offset_in))
-            id=$(jq -r '.id' < $content )
-            [[ -d incoming/$id ]] || continue
-            file=incoming/$id/$offset_in
-            [[ -s $file ]] || { 
-                    jq -r '.base64' < $content  | 
-                    base64 -d  > $file 2>/dev/null
-            }
-        fi
-        rm $content $messages
-    }&
-    req
-    ((((offset_wanted/BLOCK_SIZE)%51)==0)) && req # increase packets in flight, so its faster than blocksize/rtt 
+    if read -r offset_in id_ < <(jq -er  '.[]|select(.Content)|.Content|(.offset |tostring) +" " + .id' < $messages) && 
+        [ $id_ ]; then
+        id=${id_##*/} # security
+        #echo received $id $offset_in >&2 window $((offset_wanted - $offset_in))
+        file=incoming/$id/$offset_in
+        jq -er '.[]|select(.Content)|.Content.base64' < $messages  |
+            base64 -d  > $file 2>/dev/null  || [ . ]
+        req
+        ((((offset_wanted/BLOCK_SIZE)%101)==0)) && req # increase packets in flight, so its faster than blocksize/rtt
+    fi
+    rm -rf $messages
 done
