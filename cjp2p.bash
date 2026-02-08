b31ce6c00c88bec329f14c04a0ea41668c89d7b5
Christopher Pearson <kermit4@gmail.com>
2026-02-08 23:59:32 +0000

    attempted to make it faster.  its not really

--- cjp2p.bash
+++ cjp2p.bash
@@ -1,6 +1,7 @@
 #!/bin/bash -eu
 debug() {
-    echo "$*" >&2
+    :
+    #echo "$*" >&2
 }
 trap 'echo $0 failed at $BASH_COMMAND ;echo $0 failed at $BASH_COMMAND >&2 ' ERR
 export RUST_BACKTRACE=1
@@ -16,6 +17,7 @@ exec 11<&${COPROC[1]}
 offset_wanted=0
 eof=$2
 mkdir -p incoming/$1 # this starts the transfer
+n=0
 BLOCK_SIZE=$((0xa000))
 #[   {     "PleaseSendPeers": {}   },   {     "PleaseReturnThisMessage": {       "sent_at": 11151.745030629     }   } ]
 please_send_peers() {
@@ -28,7 +30,7 @@ please_send_peers() {
 }
 req() {
     ((offset_wanted<eof)) || return 0
-    [[ $src ]] || src=$(ls peers/ -tr|tail -20|sort -R|tail -1)
+    [[ $src ]] || src=$(find peers/ -mmin -10 -type f -printf "%T@\t%f\n" |sort -n|tail -20|sort -R|tail -1|cut -f 2 )
     debug requesting $offset_wanted from $src window $((offset_wanted - ${offset_in:-0}))
     message="[{\"PleaseSendContent\":{\"id\":\"$id\",\"length\":$BLOCK_SIZE,\"offset\":$offset_wanted}}]"
     let offset_wanted+=$BLOCK_SIZE
@@ -41,7 +43,7 @@ req() {
 }
 id=$(ls incoming/|head -1) # currrently does only one file at a time
 while true;do
-    if ! read -rt 1  src <&10    ;then # bump
+    if ! read -rt 1  -u 10 src ;then # bump
         [[ $id ]] && {
             ((offset_wanted>eof)) && 
             (($(find incoming/$id/ -mindepth 1 |wc -l) < $(((eof+BLOCK_SIZE-1)/BLOCK_SIZE)))) && {  # missing parts
@@ -60,13 +62,15 @@ while true;do
         continue
     fi
     touch peers/$src
-    read -r len <&10
-    messages=$(mktemp -p . m.XXXX)
+    read -r -u 10 len 
+    messages=m.$((n++))
     head -c $len <&10 > $messages
 #    jq -C < $messages
 #    [{"Content":{"base64":"vLTtmB1Ot1dumq1Hscila3uKZF71KU2E3mDH","eof":1073741824,"id":"1024M","offset":204791808}}]
-    if read -r offset_in id_ < <(jq -er  '.[]|select(.Content)|.Content|(.offset |tostring) +" " + .id' < $messages) && 
-        [ $id_ ]; then
+    vars=($(jq -er  '.[]|select(.Content)|.Content|(.offset |tostring) +" " + .id' < $messages)) || [ . ]
+    if [ ${vars[1]:-} ]; then
+        offset_in=${vars[0]}
+        id_=${vars[1]}
         id=${id_##*/} # security
         offset_in=${offset_in##*/} # security
         debug received $id $offset_in window $((offset_wanted - $offset_in))
